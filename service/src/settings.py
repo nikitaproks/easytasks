a18a064e60580c19b575f4c47ae490038b71532c
Mykyta Prokaiev <mykytaprokaiev@gmail.com>
2025-03-16 13:39:54 +0100

    lint

--- service/src/settings.py
+++ service/src/settings.py
@@ -26,9 +26,7 @@ class Settings(BaseSettings):
 
     @model_validator(mode="before")
     @classmethod
-    def check_card_number_not_present(
-        cls, data: dict[str, int | bool | str]
-    ) -> Any:
+    def check_card_number_not_present(cls, data: dict[str, int | bool | str]) -> Any:
         database_url = data.get("DATABASE_URL")
         if database_url:
             return data
